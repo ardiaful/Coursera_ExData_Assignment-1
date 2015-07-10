1d7332514b30da14c1ab6631b68ada42830fcaad
ardiaful <ardiaful@gmail.com>
2015-07-10 20:24:53 +0200

    First commit

--- plot3.R
+++ plot3.R
@@ -3,7 +3,7 @@ data <- read.table("household_power_consumption.txt", header=TRUE, sep=";",
                    stringsAsFactors=FALSE, na.strings=c("?"))
 
 # Subset data from big data file from two dates
-sdata <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
+subdata <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
 
 # Create a new column with the combined date and time
 subdata$datetime <- mapply(function(s1, s2){as.POSIXct(paste(s1, s2), format = "%d/%m/%Y %H:%M:%S")},subdata$Date, subdata$Time)
